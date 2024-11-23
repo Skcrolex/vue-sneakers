d3ba63f98df332b892539ce6ed715f45069649a1
Skcrolex <emilwarthunder@mail.ru>
2024-11-23 23:29:39 +0300

    Upload Vue-sneakers projects

---
+++ src/components/CardList.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Card from './Card.vue'
+
+defineProps({
+  items: Array,
+  isFavorites: Boolean,
+})
+
+const emit = defineEmits(['addToFavorite', 'addToCart'])
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-10" v-auto-animate>
+    <Card
+      v-for="item in items"
+      :key="item.id"
+      :id="item.id"
+      :title="item.title"
+      :price="item.price"
+      :imageUrl="item.imageUrl"
+      :isFavorite="item.isFavorite"
+      :onClickFavorite="isFavorites ? null : () => emit('addToFavorite', item)"
+      :onClickAdd="isFavorites ? null : () => emit('addToCart', item)"
+      :isAdded="item.isAdded"
+    />
+  </div>
+</template>
